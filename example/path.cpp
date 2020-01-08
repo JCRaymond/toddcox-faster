dc1ced7dcc7260d46faa44e247e58f8c6ace5615
allem <allemang.d@gmail.com>
2020-01-07 21:44:13 -0500

    fix path example

--- example/path.cpp
+++ example/path.cpp
@@ -1,16 +1,15 @@
-#include "tc/solver.h"
-#include "tc/groups.h"
+#include "tc/groups.hpp"
 
 #include <ctime>
 #include <iostream>
 
 int main() {
     auto cube = tc::group::B(3);
-    auto vars = tc::solve(cube, {});
+    auto vars = cube.solve();
 
-    for (int target = 1; target < vars.len; target++) {
+    for (size_t target = 1; target < vars.size(); target++) {
         auto &action = vars.path[target];
-        std::cout << action.coset << " " << action.gen << " " << target << std::endl;
+        std::cout << action.coset << " * " << action.gen << " = " << target << std::endl;
     }
 
     return 0;
